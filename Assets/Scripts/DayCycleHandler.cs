c4cc040afab5f8f591b2a179058e69de6a0e3b6b
Dat 5 Ty <Dat 5 Ty@ARTORIAS>
2025-02-27 23:54:12 +0700

    Game Manager, spawn player, daytime run in game

---
+++ Assets/Scripts/DayCycleHandler.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.UIElements;
+#endif
+
+namespace HappyHarvest
+{
+
+    /// <summary>
+    /// Handle the cycle of Day and Night. Everything that need to change across time will register itself to this handler
+    /// which will update it when it update (e.g. ShadowInstance, Interpolator etc.).
+    /// The ticking of that system can be stopped, this is useful e.g. if the game is put in pause (or need to do cutscene
+    /// etc..)
+    /// </summary>
+    [DefaultExecutionOrder(10)]
+    public class DayCycleHandler : MonoBehaviour
+    {
+        private Transform LightsRoot;
+
+        private Light2D DayLight;
+        public Gradient DayLightGradient;
+
+        private Light2D NightLight;
+        public Gradient NightLightGradient;
+
+        private Light2D AmbientLight;
+        public Gradient AmbientLightGradient;
+
+        private Light2D SunRimLight;
+        public Gradient SunRimLightGradient;
+
+        private Light2D MoonRimLight;
+        public Gradient MoonRimLightGradient;
+
+        [Tooltip("The angle 0 = upward, going clockwise to 1 along the day")]
+        public AnimationCurve ShadowAngle;
+        [Tooltip("The scale of the normal shadow length (0 to 1) along the day")]
+        public AnimationCurve ShadowLength;
+        
+
+        private void Awake()
+        {
+            GameManager.Instance.DayCycleHandler = this;
+            LightsRoot = transform;
+            DayLight = transform.Find("DayLight").GetComponent<Light2D>();
+            NightLight = transform.Find("NightLight").GetComponent<Light2D>();
+            AmbientLight = transform.Find("AmbientLight").GetComponent<Light2D>();
+            SunRimLight = transform.Find("DayLightRim").GetComponent<Light2D>();
+            MoonRimLight = transform.Find("NightLightRim").GetComponent<Light2D>();
+        }
+
+        /// <summary>
+        /// We use an explicit ticking function instead of update so the GameManager can potentially freeze or change how
+        /// time pass
+        /// </summary>
+        public void Tick()
+        {
+            UpdateLight(GameManager.Instance.CurrentDayRatio);
+        }
+
+        public void UpdateLight(float ratio)
+        {
+            DayLight.color = DayLightGradient.Evaluate(ratio);
+            NightLight.color = NightLightGradient.Evaluate(ratio);
+            AmbientLight.color = AmbientLightGradient.Evaluate(ratio);
+            SunRimLight.color = SunRimLightGradient.Evaluate(ratio);
+            MoonRimLight.color = MoonRimLightGradient.Evaluate(ratio);
+            LightsRoot.rotation = Quaternion.Euler(0,0, 360.0f * ratio);
+            UpdateShadow(ratio);
+        }
+
+        void UpdateShadow(float ratio)
+        {
+            var currentShadowAngle = ShadowAngle.Evaluate(ratio);
+            var currentShadowLength = ShadowLength.Evaluate(ratio);
+
+            var opposedAngle = currentShadowAngle + 0.5f;
+            while (currentShadowAngle > 1.0f)
+                currentShadowAngle -= 1.0f;
+        }
+        
+        public void Save(ref DayCycleHandlerSaveData data)
+        {
+            //data.TimeOfTheDay = m_CurrentTimeOfTheDay;
+        }
+        
+        public void Load(DayCycleHandlerSaveData data)
+        {
+            //m_CurrentTimeOfTheDay = data.TimeOfTheDay;
+            //StartingTime = m_CurrentTimeOfTheDay;
+        }
+    }
+
+    [System.Serializable]
+    public struct DayCycleHandlerSaveData
+    {
+        public float TimeOfTheDay;
+    }
+    
+    
+#if UNITY_EDITOR
+    // Wrapping a custom editor between UNITY_EDITOR define check allow to keep it in the same 
+    // file as this part will be stripped when building for standalone (where Editor class doesn't exist).
+    // Don't forget to also wrap the UnityEditor using at the top of the file between those define check too.
+    
+    // Show a slider that allow to test a specific time to help define colors.
+    [CustomEditor(typeof(DayCycleHandler))]
+    class DayCycleEditor : Editor
+    {
+        private DayCycleHandler m_Target;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            m_Target = target as DayCycleHandler;
+
+            var root = new VisualElement();
+            
+            InspectorElement.FillDefaultInspector(root, serializedObject, this);
+            
+            var slider = new Slider(0.0f, 1.0f);
+            slider.label = "Test time 0:00";
+            slider.RegisterValueChangedCallback((EventCallback<ChangeEvent<float>>)(evt =>
+            {
+                m_Target.UpdateLight(evt.newValue);
+                
+                slider.label = $"Test Time {DayEvent.GetTimeAsString(evt.newValue)} ({evt.newValue:F2})";
+                SceneView.RepaintAll();
+            }));
+            
+            //registering click event, it's very catch all but not way to do a change check for control change
+            root.RegisterCallback<ClickEvent>(evt =>
+            {
+                Debug.LogError(slider.value);
+                m_Target.UpdateLight(slider.value);
+                SceneView.RepaintAll();
+            });
+            
+            root.Add(slider);
+
+            return root;
+        }
+    }
+#endif
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+namespace HappyHarvest
+{
+    /// <summary>
+    /// The GameManager is the entry point to all the game system. It's execution order is set very low to make sure
+    /// its Awake function is called as early as possible so the instance if valid on other Scripts. 
+    /// </summary>
+    //[DefaultExecutionOrder(-9999)]
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager s_Instance;
+        
+        
+#if UNITY_EDITOR
+        //As our manager run first, it will also be destroyed first when the app will be exiting, which lead to s_Instance
+        //to become null and so will trigger another instantiate in edit mode (as we dynamically instantiate the Manager)
+        //so this is set to true when destroyed, so we do not reinstantiate a new one
+        private static bool s_IsQuitting = false;
+#endif
+        public static GameManager Instance 
+        {
+            get
+            {
+//#if UNITY_EDITOR
+//                if (!Application.isPlaying || s_IsQuitting)
+//                    return null;
+                
+//                if (s_Instance == null)
+//                {
+//                    //in editor, we can start any scene to test, so we are not sure the game manager will have been
+//                    //created by the first scene starting the game. So we load it manually. This check is useless in
+//                    //player build as the 1st scene will have created the GameManager so it will always exists.
+//                    Instantiate(Resources.Load<GameManager>("GameManager"));
+//                }
+//#endif
+                return s_Instance;
+            }
+        }
+        public TerrainManager Terrain { get; set; }
+        public PlayerController Player { get; set; }
+        public DayCycleHandler DayCycleHandler { get; set; }
+        public Tilemap WalkSurfaceTilemap { get; set; }
+        
+        public SceneData LoadedSceneData { get; set; }
+        
+        // Will return the ratio of time for the current day between 0 (00:00) and 1 (23:59).
+        public float CurrentDayRatio => m_CurrentTimeOfTheDay / DayDurationInSeconds;
+
+
+        [Header("Camera")]
+        public CinemachineVirtualCamera MainCamera;
+        
+        [Header("Time settings")]
+        [Min(1.0f)] 
+        public float DayDurationInSeconds;
+        public float StartingTime = 0.0f;
+
+        private bool m_IsTicking;
+        
+        private float m_CurrentTimeOfTheDay;
+
+        private void Awake()
+        {
+            s_Instance = this;
+            DontDestroyOnLoad(gameObject);
+            
+            m_IsTicking = true;
+            
+            SpawnPlayer();
+            m_CurrentTimeOfTheDay = StartingTime;
+            
+            //we need to ensure that we don't have a day length at 0, otherwise we will get stuck into infinite loop in update
+            //(and a day with 0 length makes no sense)
+            if (DayDurationInSeconds <= 0.0f)
+            {
+                DayDurationInSeconds = 1.0f;
+                Debug.LogError("The day length on the GameManager is set to 0, the length need to be set to a positive value");
+            }
+        }
+
+        private void Start()
+        {
+            m_CurrentTimeOfTheDay = StartingTime;
+        }
+
+#if UNITY_EDITOR
+        private void OnDestroy()
+        {
+            s_IsQuitting = true;
+        }
+#endif
+
+        private void Update()
+        {
+            if (m_IsTicking)
+            {
+                float previousRatio = CurrentDayRatio;
+                m_CurrentTimeOfTheDay += Time.deltaTime;
+
+                while (m_CurrentTimeOfTheDay > DayDurationInSeconds)
+                    m_CurrentTimeOfTheDay -= DayDurationInSeconds;
+            }
+            if(DayCycleHandler != null)
+            {
+                DayCycleHandler.Tick();
+            }
+        }
+        private void SpawnPlayer()
+        {
+            if(Player == null)
+            {
+                Instantiate(Resources.Load<PlayerController>("Prefabs/Character"));
+                var playerTransform = Instance.Player.transform;
+                playerTransform.position = transform.position;
+
+                Instance.MainCamera.Follow = playerTransform;
+                Instance.MainCamera.LookAt = playerTransform;
+                Instance.MainCamera.ForceCameraPosition(playerTransform.position, Quaternion.identity);
+            }
+        }
+    }
+}
